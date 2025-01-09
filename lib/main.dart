453671d424c98effbf0b2b7b7e8c2923fa6584a9
kushalmeghwal <kushalmeghwal4@gmail.com>
2025-01-09 14:19:46 +0530

    test.dart file created

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_project/Pages/ContactReadPage.dart';
 import 'package:flutter_project/Pages/HomePage.dart';
 import 'package:flutter_project/Pages/RegisterPage.dart';
 import 'package:flutter_project/Pages/WelcomePage.dart';
-import 'package:flutter_project/Util/UtilBackButton.dart';
 import 'Pages/LoginPage.dart';
 
 void main() {
@@ -11,11 +10,11 @@ void main() {
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
-      '/':(context) => MyHome(),
+      '/': (context) => MyHome(),
       'register': (context) => MyRegister(),
       'login': (context) => MyLogin(),
       'welcome': (context) => MyWelcome(),
-      'contactRead':(context)=>MyContactReadPage()
+      'contactRead': (context) => MyContactReadPage()
     },
   )); //MaterailApp
 }
